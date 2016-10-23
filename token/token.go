ae96e4827b96c3dab6f749614f2212ccfeb8ff22
Oleg Bulatov <oleg@bulatov.me>
2016-10-24 01:54:19 +0300

    Add token/token.go
    Signed-off-by: Oleg Bulatov <oleg@bulatov.me>

---
+++ token/token.go
@@ -0,0 +1,44 @@
+package token
+
+// Token is the set of lexical tokens of kbd keymap language.
+type Token int
+
+const (
+	// Special tokens
+	ILLEGAL Token = iota
+	EOL
+	EOF
+	COMMENT
+
+	CHAR
+	COMMA
+	DASH
+	EQUALS
+	LITERAL
+	NUMBER
+	PLUS
+	STR
+
+	// Keywords
+	ALT
+	ALT_GR
+	ALT_IS_META
+	AS
+	CHARSET
+	COMPOSE
+	CONTROL
+	CTRLL
+	CTRLR
+	FOR
+	INCLUDE
+	KEYCODE
+	KEYMAPS
+	PLAIN
+	SHIFT
+	SHIFTL
+	SHIFTR
+	STRING
+	STRINGS
+	TO
+	USUAL
+)
